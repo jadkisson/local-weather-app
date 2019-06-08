4e127b7ebc1f33880fa2af01646726e2b54bf2f9
Jeff Adkisson <jeff@supermassiverobot.com>
2019-06-08 19:42:41 -0400

    city search now operational

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { ICurrentWeather } from './interfaces'
+import { WeatherService } from './weather/weather.service'
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,12 @@ import { ICurrentWeather } from './interfaces'
 })
 export class AppComponent {
   currentWeather: ICurrentWeather
-  constructor() {}
+  constructor(private weatherService: WeatherService) {}
+
+  doSearch(searchValue) {
+    const userInput = searchValue.split(',').map(s => s.trim())
+    this.weatherService
+      .getCurrentWeather(userInput[0], userInput.length > 1 ? userInput[1] : undefined)
+      .subscribe(data => (this.currentWeather = data))
+  }
 }
